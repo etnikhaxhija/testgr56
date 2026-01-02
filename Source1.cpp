0656465e3e93e7d5e58a01840ce147d76e8f3bc4
eeeaz0000@gmail.com <eeeaz0000@gmail.com>
2026-01-02 23:54:42 +0100

    hapi i pare per menaxhimin e perdoruesve

---
+++ Source1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+using namespace std;
+
+class User {
+private:
+    string name;
+    string address;
